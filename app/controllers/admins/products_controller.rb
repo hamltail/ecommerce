d846d3cf59d0438e9e9f1443c339e3086cb0cbf2
wajima <goo.hiro.w@gmail.com>
2023-02-27 11:30:50 +0900

    View 作成

--- app/controllers/admins/products_controller.rb
+++ app/controllers/admins/products_controller.rb
@@ -39,6 +39,6 @@ class Admins::ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product).permit(:name, :description, :price, :stock)
+    params.require(:product).permit(:name, :description, :price, :stock, :image)
   end
 end

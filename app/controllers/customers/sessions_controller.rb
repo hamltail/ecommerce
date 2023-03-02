7fca1d44f4fb812d9188912e018e6aacd4729202
wajima <goo.hiro.w@gmail.com>
2023-03-02 15:40:35 +0900

    Controller 作成

--- app/controllers/customers/sessions_controller.rb
+++ app/controllers/customers/sessions_controller.rb
@@ -1,27 +1,21 @@
 # frozen_string_literal: true
 
 class Customers::SessionsController < Devise::SessionsController
-  # before_action :configure_sign_in_params, only: [:create]
+  before_action :reject_invalid_customer, only: [:create]
 
-  # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+  private
 
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+  def reject_invalid_customer
+    customer = Customer.find_by(email: params[:customer][:email])
+    return unless customer
 
-  # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+    return if customer.valid_password?(params[:customer][:password]) && customer.active_for_authentication?
 
-  # protected
-
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+    alert_message = if customer.status == "withdrawn"
+        "You have already resigned"
+      else
+        "Your account is suspended"
+      end
+    redirect_to request.referer, alert: alert_message
+  end
 end

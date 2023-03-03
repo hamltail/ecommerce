01fefe1319e5965a7f9c0d171592373f474f9d54
wajima <goo.hiro.w@gmail.com>
2023-03-03 15:54:33 +0900

    注文ステータスの日本語表記

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def convert_order_status(status)
+    return "入金待ち" if status === "waiting_payment"
+    return "入金確認" if status === "confirm_payment"
+    return "出荷済み" if status === "shipped"
+    return "配送中" if status === "out_of_delivery"
+    return "配達済み" if status === "delivered"
+  end
 end

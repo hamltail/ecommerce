57abfe2254ae826f21f2272d4210317d3c95bb1d
wajima <goo.hiro.w@gmail.com>
2023-03-04 11:45:54 +0900

    注文状況の表示をロールバック

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,2 @@
 module ApplicationHelper
-  # TODO: FIXME: カラムにコードを保持していないのでNG。この処理も紛らわしい
-  def convert_order_status(status)
-    return "入金待ち" if status === "waiting_payment"
-    return "入金確認" if status === "confirm_payment"
-    return "出荷済み" if status === "shipped"
-    return "配送中" if status === "out_of_delivery"
-    return "配達済み" if status === "delivered"
-  end
 end

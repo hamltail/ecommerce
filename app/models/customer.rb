11dd859b00102aee1011659c858642824191baa6
wajima <goo.hiro.w@gmail.com>
2023-03-04 13:16:58 +0900

    Customer の Validation 修正

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,15 +4,20 @@ class Customer < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  with_options presence: true do
-    validates :name
-    validates :status
-  end
+  validates :name,
+            uniqueness: true,
+            presence: true,
+            length: { minimum: 2, maximum: 40 }
+  validates :email,
+            uniqueness: true,
+            presence: true
+  validates :status,
+            presence: true
 
   enum status: {
     normal: 0,
     withdrawn: 1,
-    banned: 2
+    banned: 2,
   }
 
   has_many :cart_items, dependent: :destroy
@@ -23,20 +28,20 @@ class Customer < ApplicationRecord
       {
         quantity: cart_item.quantity,
         price_data: {
-          currency: 'jpy',
+          currency: "jpy",
           unit_amount: cart_item.product.price,
           product_data: {
             name: cart_item.product.name,
             metadata: {
-              product_id: cart_item.product_id
-            }
-          }
-        }
+              product_id: cart_item.product_id,
+            },
+          },
+        },
       }
     end
   end
 
   def active_for_authentication?
-    super && (status == 'normal')
+    super && (status == "normal")
   end
 end

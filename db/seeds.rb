0cacd734517c45eace6e301006d4fc4d271538a7
wajima <goo.hiro.w@gmail.com>
2023-03-25 17:27:14 +0900

    seed 変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,109 +24,109 @@ p '==================== product create ===================='
 description = "原産地の一つとされるスペインバレンシアで、祖父の代から3代に渡り生産する老舗が、手間暇かけて生産した貴重な製品をスペインから直送します。モミの木で作った、厚さ3mmのずっしりと重みのある木箱に入れてお届け。木箱と製品のコントラストが、プレゼントの華やかさをグッと際立てます。\nかつてはスペイン王室にも献上し、今では毎年1,000箱以上もスペイン国外へ販売されるこの製品の素晴らしさは言うまでもありません。\n大量生産が出来ない製品のため、数量限定での販売となっています。ぜひお早めにお申し込みください。"
 
 product1 = Product.new(
-  name: "イエロー ハット",
+  name: "小樽ワイン ナイアガラ",
   description: description, 
   price: 2500, 
   stock: 30
 )
-product1.image.attach(io: File.open(Rails.root.join('app/assets/images/hat.png')), filename: 'hat.png')
+product1.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product1.save!
 
 product2 = Product.new(
-  name: "エレガント ジャケット",
+  name: "小樽ワイン キャンベルアーリー",
   description: description, 
-  price: 34800, 
+  price: 3480, 
   stock: 21
 )
-product2.image.attach(io: File.open(Rails.root.join('app/assets/images/jacket.png')), filename: 'jacket.png')
+product2.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product2.save!
 
 product3 = Product.new(
-  name: "ぬくもり パンツ",
+  name: "余市ワイン ナイアガラ",
   description: description, 
   price: 2980, 
   stock: 30
 )
-product3.image.attach(io: File.open(Rails.root.join('app/assets/images/pants.png')), filename: 'pants.png')
+product3.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product3.save!
 
 product4 = Product.new(
-  name: "T-シャツ",
+  name: "フランスワイン ボジョレーヌーボー",
   description: description, 
   price: 4900, 
   stock: 120
 )
-product4.image.attach(io: File.open(Rails.root.join('app/assets/images/shirt.png')), filename: 'shirt.png')
+product4.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product4.save!
 
 product5 = Product.new(
-  name: "高級 スリッパ",
+  name: "ドン ペリニヨン",
   description: description, 
   price: 99800, 
   stock: 10
 )
-product5.image.attach(io: File.open(Rails.root.join('app/assets/images/shoes.png')), filename: 'shoes.png')
+product5.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product5.save!
 
 product6 = Product.new(
-  name: "ビビッド スカート",
+  name: "イタリアワイン",
   description: description, 
   price: 3800, 
   stock: 50
 )
-product6.image.attach(io: File.open(Rails.root.join('app/assets/images/skirt.png')), filename: 'skirt.png')
+product6.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product6.save!
 
 product7 = Product.new(
-  name: "食べ比べセット【日本産】黒毛和牛",
+  name: "チリワイン 赤",
   description: description, 
-  price: 33300, 
-  stock: 10
+  price: 500, 
+  stock: 1000
 )
-product7.image.attach(io: File.open(Rails.root.join('app/assets/images/niku_japan.png')), filename: 'niku_japan.png')
+product7.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product7.save!
 
 product8 = Product.new(
-  name: "食べ比べセット【アメリカ産】コスパ良好",
+  name: "チリワイン 白",
   description: description, 
-  price: 12000, 
-  stock: 30
+  price: 500, 
+  stock: 300
 )
-product8.image.attach(io: File.open(Rails.root.join('app/assets/images/niku_america.png')), filename: 'niku_america.png')
+product8.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product8.save!
 
 product9 = Product.new(
-  name: "食べ比べセット【中国産】質より量",
+  name: "アメリカワイン",
   description: description, 
-  price: 6000, 
+  price: 2000, 
   stock: 100
 )
-product9.image.attach(io: File.open(Rails.root.join('app/assets/images/niku_china.png')), filename: 'niku_china.png')
+product9.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product9.save!
 
 product10 = Product.new(
-  name: "食べ比べセット【フランス産】記念日に",
+  name: "中国ワイン",
   description: description, 
-  price: 38000, 
-  stock: 20
+  price: 300, 
+  stock: 200
 )
-product10.image.attach(io: File.open(Rails.root.join('app/assets/images/niku_france.png')), filename: 'niku_france.png')
+product10.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product10.save!
 
 product11 = Product.new(
-  name: "食べ比べセット【イタリア産】記念日に",
+  name: "高級イタリアワイン",
   description: description, 
   price: 24800, 
   stock: 30
 )
-product11.image.attach(io: File.open(Rails.root.join('app/assets/images/niku_italy.png')), filename: 'niku_italy.png')
+product11.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product11.save!
 
 product12 = Product.new(
-  name: "食べ比べセット【ジンバブエ産】",
+  name: "高級フランスワイン",
   description: description, 
-  price: 1, 
-  stock: 1
+  price: 36800, 
+  stock: 10
 )
-product12.image.attach(io: File.open(Rails.root.join('app/assets/images/niku_zimbabwe.png')), filename: 'niku_zimbabwe.png')
+product12.image.attach(io: File.open(Rails.root.join('app/assets/images/wine01.png')), filename: 'wine01.png')
 product12.save!
